52f4cca76935e248c28ff93c2485970071694f68
kongarap <kongarap@mail.sacredheart.edu>
2024-04-06 23:14:32 -0400

    first commit

---
+++ Classwork1.R
@@ -0,0 +1,65 @@
+# Printing Hello World
+"Hello, World"
+# Printing Sum Of Operations
+A <- 5
+B <- 10
+C <-A+B
+C
+# Printing Quotient Of Operation
+v1 <- 4.5
+v2 <- 2
+v3 <- v1/v2
+v3
+v4 <- sqrt(v1)
+v4
+# Printing More Variables
+greet <- "Hello"
+name <- "Prashanthi"
+message <- paste(greet, name)
+message
+# Printing Type of data
+x <- 2L
+typeof(x)
+y <- 2.5
+typeof(y)
+# Printing data type
+y <- 2.5
+typeof(y)
+# Printing Dtype
+z <- 2+3i
+typeof(z)
+a <- "h"
+typeof(a)
+q <- T
+typeof(q)
+p <- FALSE
+typeof(p)
+# Printing Basic Math using a,b,c,d,e,f operators
+a <- 5
+b <- 3
+c <- a+b
+d <- a-b
+e <- a*b
+f<- a/b
+a
+b
+c
+d
+e
+f
+# Printing basic logic using sic operations and complex operations
+4 <5
+10>100
+4==5
+result <- 4 < 5
+result
+typeof(result)
+result <- 4 < 5
+result
+typeof(result)
+result2 <- !(5>1)
+result2
+result | result2
+result & result2
+isTRUE(result)
+
