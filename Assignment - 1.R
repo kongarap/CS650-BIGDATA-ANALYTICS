4ddd2630f06cec51cab4c7c299c6f755807f72ac
kongarap <166187491+kongarap@users.noreply.github.com>
2024-06-22 18:46:20 -0400

    assignment

---
+++ Assignment - 1.R
@@ -0,0 +1,37 @@
+# Printing the average of 3 numbers
+num1 <- 7
+num2 <- 12
+num3 <- 5
+# Calculate sum of 3 numbers
+Total <- num1 + num2 + num3
+# Calculate the average
+average <- Total/3
+average
+# Printing a program "Welcome to R",along with my first name
+greet <- "Welcome to R"
+# Defining my first name
+first_name <- "Prashanthi"
+message <- paste(greet, first_name)
+message
+# Assign values to variables x, y, and z
+x <- 7
+y <- 12
+z <- 4
+# Check if x is bigger than y
+if (x > y) {
+  print("x is bigger than y.")
+} else {
+  print("x is not bigger than y.")
+}
+# Check if x is bigger than z
+if (x > z) {
+  print("x is bigger than z.")
+} else {
+  print("x is not bigger than z.")
+}
+
+
+
+
+
+
